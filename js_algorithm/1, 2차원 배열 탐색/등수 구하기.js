1794ce67ea7f93131a26f183a5aec0b76bd9af1e
benkim3858 <benkim3858@gmail.com>
2022-09-07 00:52:17 +0900

    등수 구하기

--- js_algorithm/1, 2차원 배열 탐색/등수 구하기.js
+++ js_algorithm/1, 2차원 배열 탐색/등수 구하기.js
@@ -1,12 +1,12 @@
 function solution(arr) {
-    let answer = [];
-    for(let i = 0; i < arr.length; i++) {
-        if(arr[i-1] < arr[i]) {
-            console.log(arr[i-1])
-            console.log(arr[i]);
-            answer.push(i+1)
-        } else {
-            answer.push(i+2)
+    const n = arr.length;
+    let answer = Array.from({length:n}, () => 1);
+
+    for(let i = 0; i < n; i++) {
+        for(let j = 0; j <n; j++) {
+            if(arr[i] < arr[j]) {
+                answer[i]++;
+            }
         }
     }
 
