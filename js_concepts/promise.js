a19ca467b3d0bd6c040215da62907da12a0c3cf7
benkim3858 <benkim3858@gmail.com>
2022-11-13 14:22:25 +0900

    promise

--- js_concepts/promise.js
+++ js_concepts/promise.js
@@ -1,1 +1,23 @@
 // Promise
+const render = () => console.log('rendering...');
+
+const timeout = time => (resolve, reject) => {
+    setTimeout(() => resolve('promise : '), time);
+}
+
+const delay = time => new Promise(timeout(time));
+
+delay(3000)
+    .then((msg) => {
+        console.log(msg + '처리중');
+        return delay(2000);
+    })
+    .then((msg) => {
+        console.log(msg + '여전히');
+        return delay(1000)
+    })
+    .then((msg) => {
+        console.log(msg + '곧완료');
+        render();
+    });
+
